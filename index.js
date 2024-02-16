fdd9e9b67b4ea642a8fb5d1987a54501b7237cf0
Tadeáš <taddy.fort@gmail.com>
2024-02-16 22:42:38 +0100

    Remove unused files and update .gitignore

--- index.js
+++ index.js
@@ -17,61 +17,26 @@ async function connectToMongo() {
   await client.connect();
   console.log("Connected to MongoDB");
   const db = client.db("eve-killboard");
-  const killsCollection = db.collection("Kills");
-  return killsCollection;
+  return db;
 }
 
-const killsCollectionPromise = connectToMongo();
+const dbPromise = connectToMongo();
 
-const characters = [
-  { id: "1772807647", name: "Tadeas CZ" },
-  { id: "2119522407", name: "7oXx" },
-  { id: "1296770674", name: "Emnar Thidius" },
-  { id: "94370897", name: "Richard Valdyr" },
-  { id: "813634421", name: "SanZo Fengi" },
-  { id: "135683356", name: "Zeerover" },
-  { id: "1902051600", name: "ZeusCommander" },
-  { id: "93985921", name: "Vilzuh" },
-  { id: "1107376792", name: "snipereagle1" },
-  { id: "1787233431", name: "Ice Maniac" },
-  { id: "885978821", name: "Bruch Wayne" },
-  { id: "1702746983", name: "cegg" },
-  { id: "211748991", name: "doombreed52" },
-  { id: "2116613377", name: "HelloMeow" },
-  { id: "1122768769", name: "Holo Mez" },
-  { id: "95859739", name: "Jimmy Oramara" },
-  { id: "91412054", name: "K Sully" },
-  { id: "91322151", name: "Nokin Niam" },
-  { id: "95618389", name: "Phoenix Snow" },
-  { id: "1528387696", name: "RawNec" },
-  { id: "418281780", name: "Seth Quado" },
-  { id: "1181492764", name: "SgtSlacker" },
-  { id: "2117414873", name: "Tec8n0" },
-  { id: "2120058366", name: "TheRealFatback" },
-  { id: "2114249907", name: "Tion Galler" },
-  { id: "92663124", name: "Sville Sveltos" },
-  { id: "2115781306", name: "Aretha LouiseFrank" },
-  { id: "2113791254", name: "Cleanthes" },
-  { id: "2112599464", name: "Mad Dawg Yaken" },
-  { id: "1451471232", name: "mr bowjangles" },
-  { id: "2116105023", name: "Drithi Moonshae" },
-  { id: "2120186660", name: "ozzy993" },
-  { id: "134063007", name: "Yamcha7" },
-  { id: "93426904", name: "John Cravius" },
-  { id: "91613448", name: "Private Panacan" },
-  { id: "1135028350", name: "Arkady Drayson" },
-  { id: "93466458", name: "Malcolm Bobodiablo" },
-  { id: "91290222", name: "tainted demon" },
-  { id: "345875676", name: "Caleb Drakka" },
-];
+// ! RÁN killboard core
+
+async function fetchCharacters() {
+  const db = await dbPromise;
+  const charactersCollection = db.collection("characters");
+  return charactersCollection.find({}).toArray();
+}
 
 async function fetchWithRetry(url, retries = 3, delay = 3000) {
   for (let i = 0; i < retries; i++) {
     try {
       const response = await axios.get(url);
-      return response.data; // Success
+      return response.data;
     } catch (error) {
-      if (i === retries - 1) throw error; // Exhausted retries, throw error
+      if (i === retries - 1) throw error;
       console.log(`Request failed, retrying (${i + 1}/${retries})...`);
       await new Promise((resolve) => setTimeout(resolve, delay));
     }
@@ -80,7 +45,10 @@ async function fetchWithRetry(url, retries = 3, delay = 3000) {
 
 async function fetchAndStoreKillData() {
   try {
-    const killsCollection = await killsCollectionPromise;
+    const db = await dbPromise;
+    const killsCollection = db.collection("Kills");
+    const characters = await fetchCharacters();
+
     console.log("Fetching kill data");
 
     const now = new Date();
@@ -88,7 +56,7 @@ async function fetchAndStoreKillData() {
     const firstDayOfWeek = new Date(
       now.setDate(now.getDate() - (dayOfWeek === 0 ? 6 : dayOfWeek - 1))
     );
-    firstDayOfWeek.setHours(0, 0, 0, 0); // Set to the start of the day
+    firstDayOfWeek.setHours(0, 0, 0, 0);
 
     for (const character of characters) {
       const zKillUrl = `https://zkillboard.com/api/kills/characterID/${character.id}/`;
@@ -149,13 +117,11 @@ app.get("/", (req, res) => {
 app.get("/api/weekly-summary", async (req, res) => {
   try {
     const killsCollection = await killsCollectionPromise;
-    // Now you can use killsCollection for aggregation directly without connecting again
-    // Calculate the start and end of the current week
     const now = new Date();
     const startOfWeek = new Date(now.setDate(now.getDate() - now.getDay()));
-    startOfWeek.setHours(0, 0, 0, 0); // Start of the week
+    startOfWeek.setHours(0, 0, 0, 0);
     const endOfWeek = new Date(now.setDate(now.getDate() - now.getDay() + 6));
-    endOfWeek.setHours(23, 59, 59, 999); // End of the week
+    endOfWeek.setHours(23, 59, 59, 999);
 
     const aggregation = [
       {
@@ -203,6 +169,44 @@ app.get("/api/weekly-summary", async (req, res) => {
     console.error("Error fetching weekly summary:", error);
     res.status(500).send("Internal Server Error");
   }
+});
+
+// ! price fetching
+async function updatePrices() {
+  const db = await dbPromise;
+  const collection = db.collection("Prices");
+
+  // Check if the collection exists and is not empty
+  const count = await collection.countDocuments();
+  if (count > 0) {
+    // Remove all documents if the collection is not empty
+    await collection.deleteMany({});
+  }
+
+  // Fetch new prices data
+  const response = await axios.get(
+    "https://esi.evetech.net/latest/markets/prices/?datasource=tranquility"
+  );
+  const pricesData = response.data.map((item) => ({
+    ...item,
+    last_updated: new Date(),
+  }));
+
+  // Insert new prices data into the collection
+  await collection.insertMany(pricesData);
+  console.log("Prices collection updated.");
+}
+
+// Schedule the task to run every 24 hours for updating prices
+cron.schedule("0 0 * * *", () => {
+  console.log("Running a task every 24 hours to update prices");
+  updatePrices().catch(console.error);
+});
+
+// Add an endpoint to manually trigger the prices update
+app.get("/api/update-prices", async (req, res) => {
+  await updatePrices();
+  res.send("Prices collection has been updated.");
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
